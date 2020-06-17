343d0946386f4ab7398334df1e4f8a5cac9befd0
Rishit123344 <bramarasunku@gmail.com>
2020-06-17 15:52:34 +0530

    QR scanner

--- Screens/BookTransactionScreen.js
+++ Screens/BookTransactionScreen.js
@@ -1,12 +1,59 @@
 import React from 'react'
-import {Text,View} from 'react-native'
+import {Text,View,TouchableOpacity,StyleSheet} from 'react-native'
+import * as Permissions from 'expo-permissions'
+import {BarCodeScanner} from 'expo-barcode-scanner'
 
 export default class TransactionScreen extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            hasCameraPermissions:null,
+            scanned:false,
+scannedData:"",
+buttonState:'normal'
+        }
+    }
+    getPermissionsAsync = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermissions: status === 'granted',buttonState:'clicked',scanned:false});
+      };
+      handleBarCodeScanned=async({type,data})=>{
+          this.setState({
+              scanned:true,
+              scannedData:data,
+              buttonState:'normal'
+          })
+      }
     render(){
+        const hasCameraPermissions=this.state.hasCameraPermissions;
+        const scanned = this.state.scanned;
+        const buttonState = this.state.buttonState;
+        if (buttonState==='clicked'&&hasCameraPermissions){
+            return(
+               <BarCodeScanner onBarCodeScanned={scanned ?undefined:this.handleBarCodeScanned}style={StyleSheet.absoluteFillObject}></BarCodeScanner>
+            )
+        }
+        else if(buttonState==='normal'){
+
+        
 return(
 <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-<Text>Issue or return</Text>
+<Text style={{fontSize:15,textDecorationLine:'underline'}}>{hasCameraPermissions===true ? this.state.scannedData:"request camera permissions"}</Text>
+<TouchableOpacity style={styles.ScanButton}onPress={this.getPermissionsAsync}>
+<Text style={styles.buttontext}>Scan QR Code</Text>
+</TouchableOpacity>
 </View>
 );
+        }
     }
 }
+const styles = StyleSheet.create({
+    buttontext:{
+        fontSize:20
+    },
+    ScanButton:{
+        backgroundColor:"blue",
+        padding:10,
+        margin:10
+    },
+})
